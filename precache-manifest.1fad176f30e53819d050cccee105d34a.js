1f401615b980fc6f1eb1be9bdf574b1c87fd6149
hieudao-connectiv <hieu.dao.connectiv@gmail.com>
2020-12-28 13:45:33 +0700

    Updates

---
+++ precache-manifest.1fad176f30e53819d050cccee105d34a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e9ed587d302f8b76b64e784fa17219",
+    "url": "/basic-web/index.html"
+  },
+  {
+    "revision": "56670d764beb0b994b8c",
+    "url": "/basic-web/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "8fb12fc32806b3b16f9d",
+    "url": "/basic-web/static/css/main.5b1f7f80.chunk.css"
+  },
+  {
+    "revision": "56670d764beb0b994b8c",
+    "url": "/basic-web/static/js/2.2629ea02.chunk.js"
+  },
+  {
+    "revision": "8fb12fc32806b3b16f9d",
+    "url": "/basic-web/static/js/main.7cfb1e2d.chunk.js"
+  },
+  {
+    "revision": "1bcf83eba2fad4bcbd6e",
+    "url": "/basic-web/static/js/runtime-main.49a88b05.js"
+  },
+  {
+    "revision": "8af7166cd3202d483ce9ed62a556c91a",
+    "url": "/basic-web/static/media/bg.8af7166c.jpg"
+  }
+]);
